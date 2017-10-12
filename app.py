abda9dff27008ec05b880278e7a779d28f108bdb
Viktor Kobryn <vkobryn@lohika.com>
2017-10-12 15:33:26 +0300

    change station name to id

--- app.py
+++ app.py
@@ -15,34 +15,34 @@ class Metadata:
         self.stations = {}
         self.tz = pytz.timezone('Europe/Uzhgorod')
 
-    def add_song(self, station_name, song_name, play_from):
-        if not self.stations.get(station_name):
-            self.stations[station_name] = []
-        if len(self.stations[station_name]) == self.max_songs:
-            del self.stations[station_name][0]
-        self.stations[station_name].append(
+    def add_song(self, station_id, song_name, play_from):
+        if not self.stations.get(station_id):
+            self.stations[station_id] = []
+        if len(self.stations[station_id]) == self.max_songs:
+            del self.stations[station_id][0]
+        self.stations[station_id].append(
             {'name': song_name,
              'play_from': play_from or '%s:%s' % (
                  datetime.datetime.now(self.tz).hour, datetime.datetime.now(self.tz).minute)})
 
-    def get_songs(self, station_name):
-        return self.stations.get(station_name, {})
+    def get_songs(self, station_id):
+        return self.stations.get(station_id, {})
 
 metadata = Metadata()
 
 
-@app.route('/metadata/add/<string:station_name>/<string:song_name>', methods=['POST', 'GET'])
-def metadata_add(station_name, song_name):
+@app.route('/metadata/add/<string:station_id>/<string:song_name>', methods=['POST', 'GET'])
+def metadata_add(station_id, song_name):
     if request.args.get('username', '') != config.USER_NAME \
             or request.args.get('password', '') != config.USER_PASSWORD:
         abort(403)
-    metadata.add_song(station_name, song_name, request.args.get('play_from', None))
+    metadata.add_song(station_id, song_name, request.args.get('play_from', None))
     return 'OK'
 
 
-@app.route('/metadata/get/<string:station_name>')
-def metadata_get(station_name):
-    meta = metadata.get_songs(station_name)
+@app.route('/metadata/get/<string:station_id>')
+def metadata_get(station_id):
+    meta = metadata.get_songs(station_id)
     return json.dumps(meta)
 
 if __name__ == '__main__':
